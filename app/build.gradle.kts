6b4d613d76656c14a625afc55c3a54c82705ef1c
madroots <simon.barker@linuxmail.org>
2026-07-22 17:26:14 +0200

    Icon: mipmap resources with proper sizes + adaptive icon XML for TV launcher (v3.9.7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.jw.tv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 59
-        versionName = "3.9.6"
+        versionCode = 60
+        versionName = "3.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
